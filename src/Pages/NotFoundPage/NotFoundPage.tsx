8f1ac91416856cd631d77ab80981bb260dcf7e46
Carlos Yoshio Tsuda <cyt231@gmail.com>
2022-02-17 16:06:44 -0300

    Add not found page

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { useNavigate } from "react-router-dom"
+
+// Ant Design
+import { Result, Button } from "antd";
+
+// Classes & Styles
+import classes from "./NotFoundPage.module.less";
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Not found page
+    <div className={classes.container}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={() => navigate(-1)}>Back</Button>
+        }
+      />
     </div>
+
   );
 }
 
